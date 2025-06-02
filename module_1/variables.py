7f88726dedc2c5d36aa93ff2d2664ef301d5b6ad
Enes <enesnimani87@gmai.com>
2025-06-02 17:50:07 +0200

    asd

---
+++ module_1/variables.py
@@ -0,0 +1,24 @@
+temperature = 13
+name = "Enes"
+
+print(type(temperature))
+
+print(type(name))
+
+x = 10
+y = 20
+z = 10 + 20
+print(x + y)
+print(x - y)
+print(x / y)
+print(x * y)
+
+print(z)
+
+age = 30
+age = age + 1
+
+firstname = "Jon"
+surname = "Bekolli"
+
+print(firstname + " " + surname)
