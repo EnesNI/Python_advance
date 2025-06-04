9eb67ea98960e04a3580597a7eebf46c37eaea14
Student <enesnimani87@gmail.com>
2025-06-04 17:08:29 +0200

    asd

---
+++ module_1/list.py
@@ -0,0 +1,27 @@
+name_of_the_list=["item1","item","item3"]
+to_do_list=['match_homework','research for bio','clean the room','fun']
+
+first_item=to_do_list[0]
+seacend_item=to_do_list[1]
+third_item=to_do_list[2]
+fourth_item=to_do_list[3]
+
+print(first_item)
+
+
+favorite_color=['red','blue','yellow','green']
+print(favorite_color[2])
+
+shopping_list=['apples','orange',3,4.5]
+
+# shopping_list.attand
+shopping_list.insert(2, "milk")
+
+to_do_list.remove('match_homework')
+print(to_do_list)
+
+del shopping_list[1]
+
+
+to_do_list[1]="research math"
+print(to_do_list)
