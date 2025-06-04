4fce6a04eb1a97723aa980f8996c586f0403379e
Student <enesnimani87@gmail.com>
2025-06-04 17:36:10 +0200

    asd123

---
+++ module_2/tuple.py
@@ -0,0 +1,11 @@
+words =('spam','eggs','milk')
+
+print(words[0])
+
+
+
+person=('Amar',14,'Student')
+
+name,age,profession=person
+
+print(name,"'s", "profession is",profession,"and he is",age,"years old")
