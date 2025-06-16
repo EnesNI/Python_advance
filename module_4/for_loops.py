a57ca254c5492dde06c4aed7560ba2eea2629759
Student <enesnimani87@gmail.com>
2025-06-16 17:45:14 +0200

    123

---
+++ module_4/for_loops.py
@@ -0,0 +1,13 @@
+names = ["Amari", "Blerdoni", "Ylli", "Ernesi", "Enesi", "Donika"]
+
+for name in names:
+    print(name)
+
+sentence = "Hello, World"
+for character in sentence:
+    if character.isalpha():
+          print(character)
+
+          #range function
+          for number in range(1, 10):
+              print(number)
