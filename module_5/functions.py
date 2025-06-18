33deb9279a7fa7222a237449687c071745750efd
Student <enesnimani87@gmail.com>
2025-06-18 17:46:12 +0200

    123

---
+++ module_5/functions.py
@@ -0,0 +1,16 @@
+def greet():
+    print("Hello world")
+
+greet()
+
+
+def greet_person(name):
+    print("Hello, ", name)
+
+greet_person("Amar")
+
+def sum(x, y):
+    z = x + y
+    return z
+
+print(sum(10, 7))
